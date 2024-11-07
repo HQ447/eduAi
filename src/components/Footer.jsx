55e479dc310fed19680c70e178cffa0b625c85bc
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-07 18:41:08 +0500

    7/11

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,31 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col px-20 py-10 gap-5">
+      <div className="flex justify-between mb-3 flex-wrap gap-10 -sm:px-10 -sm:justify-center">
+        <div className="flex flex-col gap-4 ">
+          <h1 className=" text-xl font-bold">About</h1>
+          <h1>Our Story</h1>
+          <h1>Privacy Policy</h1>
+          <h1>FAQ's</h1>
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <h1 className=" text-xl font-bold">Quick Links</h1>
+          <h1>Courses</h1>
+          <h1>My Account</h1>
+          <h1>Cource Dashboard</h1>
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <h1 className=" text-xl font-bold">Social Links</h1>
+          <h1>Youtube</h1>
+          <h1>Instagram</h1>
+          <h1>Github</h1>
+        </div>
+      </div>
+      <p className="w-full text-center">
+        Copyright © 2024 eduAI | All Rights Reserved
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className=" py-10 flex flex-col px-20  bg-[#cce7f5] w-full">
       <div className="flex w-full  min-h-screen justify-center items-center -lg:flex-wrap-reverse">
-        <div className=" flex w-2/4">
+        <div className="flex basis-3/6 justify-center">
           <h1 className="text-5xl">
             Start your{" "}
             <span
@@ -18,11 +18,11 @@ function Home() {
             with our Dedicated community!
           </h1>
         </div>
-        <div className=" flex  w-2/4">
+        <div className="flex basis-3/6 justify-center">
           <img
             src="https://www.becodemy.com/_next/static/media/banner.8a9f498b.svg"
             alt="img loading error"
-            className="w-full"
+            className="w-[30rem] -md:w-[50rem]"
           />
         </div>
       </div>
