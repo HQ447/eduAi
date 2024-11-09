2f762f47c0b8c4624e5fa325b7c3223ce695c627
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-09 18:58:09 +0500

    9/11

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,25 @@ import { NavLink } from "react-router-dom";
 function Login() {
   return (
     <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
+      <div className="w-fit flex flex-col px-3 py-3 rounded-lg  ">
+        <h1 className="text-xl font-semibold">Hello!</h1>
+        <p>Welcome back</p>
+        <div>
+          <input type="text" placeholder="Enter Email" />
+          <input type="password" placeholder="Password" />
+          <p>Recovery Password</p>
+        </div>
+        <button>Sign in</button>
+
+        <div>
+          <p>or continue with</p>
+          <div className="flex "></div>
+          <p>
+            Not a member?<a href="">Register now</a>
+          </p>
+        </div>
+      </div>
+
       {/* <div className=" w-fit flex justify-center items-center gap-8 flex-col bg-white px-20 -md:px-16 -sm:px-8 -xsm:px-2 -xsm:py-3 py-10 -xsm:w-11/12 rounded-lg">
 
         <h1 className="w-fit text-center shadow-lg rounded-full px-9 py-1 bg-[#fbb329] text-xl ">
