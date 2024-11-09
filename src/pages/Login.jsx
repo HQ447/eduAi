892f4ade7af375cd9269356c213c99fb12f2ed89
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-09 19:51:04 +0500

    9/11

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ import { FaFacebook } from "react-icons/fa";
 function Login() {
   return (
     <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
-      <div className=" w-[28%] flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
+      <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
         <h1 className="text-2xl text-center font-semibold">Hello!</h1>
         <p className="text-md text-center">Welcome back</p>
         <div className="flex flex-col my-3 gap-4">
