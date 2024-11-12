e2f89d93ee65e2b2443eb9b10fce5b89957f7f4a
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-12 13:43:59 +0500

    10/11

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -11,6 +11,11 @@ function Login() {
   return (
     <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
       <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Student_icon.svg/1024px-Student_icon.svg.png"
+          alt=""
+          className="w-14 h-14 rounded-full mb-2 mx-auto"
+        />
         <h1 className="text-2xl text-center font-semibold">Hello!</h1>
         <p className="text-md text-center">Welcome back</p>
         <div className="flex flex-col my-3 gap-4">
@@ -29,7 +34,7 @@ function Login() {
           </p>
         </div>
         <NavLink to={"/"} className={"w-full"}>
-          <button className="w-full bg-orange-500 mb-3 hover:scale-95 transition-all hover:bg-orange-600 text-white rounded-md py-2 shadow-md">
+          <button className="w-full bg-[#653bce] mb-3 hover:scale-95 transition-all hover:bg-orange-600 text-white rounded-md py-2 shadow-md">
             Sign in
           </button>
         </NavLink>
--- src/pages/auth/RegisterUser.jsx
+++ src/pages/auth/RegisterUser.jsx
@@ -10,7 +10,12 @@ import { NavLink } from "react-router-dom";
 function RegisterUser() {
   return (
     <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
-      <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
+      <div className="  w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Student_icon.svg/1024px-Student_icon.svg.png"
+          alt=""
+          className="w-14 h-14 rounded-full mb-2 mx-auto"
+        />
         <h1 className="text-2xl text-center font-semibold">Hello!</h1>
         <p className="text-md text-center">Register Youself Here!</p>
         <div className="flex flex-col my-3 gap-4">
@@ -31,7 +36,7 @@ function RegisterUser() {
           />
         </div>
         <NavLink to={"/"} className={"w-full"}>
-          <button className="w-full bg-orange-500 mb-3 hover:scale-95 transition-all hover:bg-orange-600 text-white rounded-md py-2 shadow-md">
+          <button className="w-full bg-[#653bce] mb-3 hover:scale-95 transition-all hover:bg-orange-600 text-white rounded-md py-2 shadow-md">
             Register
           </button>
         </NavLink>
