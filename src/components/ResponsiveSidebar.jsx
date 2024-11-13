7d4a0e928e00162ec4a1fdcb40cb1ecc1b217e14
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-13 17:58:17 +0500

    10/11

---
+++ src/components/ResponsiveSidebar.jsx
@@ -0,0 +1,14 @@
+function ResponsiveSidebar() {
+  return (
+    <div className="min-h-screen flex w-full fixed top-0 z-50">
+      <div className="w-[70%] min-h-screen px-7 gap-4 flex flex-col bg-black">
+        <div>Link 1</div>
+        <div>Link 1</div>
+        <div>Link 1</div>
+        <div>Link 1</div>
+      </div>
+    </div>
+  );
+}
+
+export default ResponsiveSidebar;
