f8a1fc8a90b06ede20e8846e27bc83354cb1afcb
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-13 18:21:54 +0500

    10/11

--- src/components/ResponsiveSidebar.jsx
+++ src/components/ResponsiveSidebar.jsx
@@ -1,12 +1,10 @@
 function ResponsiveSidebar() {
   return (
-    <div className="min-h-screen flex bg-[#00000097]  w-full fixed top-0 z-50">
-      <div className="-md:w-[50%] -sm:w-[60%] -xsm:w-[80%] min-h-screen px-7   text-white gap-4 flex flex-col bg-black">
-        <div>Link 1</div>
-        <div>Link 1</div>
-        <div>Link 1</div>
-        <div>Link 1</div>
-      </div>
+    <div className="px-10 justify-center  gap-5 flex-col min-h-screen -md:w-[50%] -sm:w-[60%] -xsm:w-[80%] flex bg-[white]  fixed top-0 right-0 z-30">
+      <div>Link 1</div>
+      <div>Link 1</div>
+      <div>Link 1</div>
+      <div>Link 1</div>
     </div>
   );
 }
