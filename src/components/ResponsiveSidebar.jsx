f1d42e31a90d0c250e956e781d9ca88c26a2d805
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-13 20:05:51 +0500

    10/11

--- src/components/ResponsiveSidebar.jsx
+++ src/components/ResponsiveSidebar.jsx
@@ -5,6 +5,7 @@ import { IoHomeOutline } from "react-icons/io5";
 import { GiBlackBook } from "react-icons/gi";
 import { AiOutlineTeam } from "react-icons/ai";
 import { IoBulbOutline } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 
 function ResponsiveSidebar() {
   const dispatch = useDispatch();
@@ -29,7 +30,7 @@ function ResponsiveSidebar() {
       <div
         className={`relative ${showSidebar ? "right-0" : "-right-[50rem]"} ${
           darkMode ? "!bg-[#141414fc] text-white" : "bg-white"
-        } px-20 justify-center  transition-all gap-5 flex-col min-h-screen -md:w-[50%] -sm:w-[60%] -xsm:w-[80%] flex z-50 `}
+        } px-20 -xsm:px-10 justify-center  transition-all gap-10 flex-col min-h-screen -md:w-[50%] -sm:w-[60%] -xsm:w-[80%] flex z-50 `}
       >
         <RxCross2
           className={`${
@@ -37,22 +38,59 @@ function ResponsiveSidebar() {
           } absolute top-10 left-10 text-2xl`}
           onClick={() => dispatch(updateShowSidebar(false))}
         />
-        <div className="flex gap-3 items-center">
-          <IoHomeOutline />
+        <div className="w-full gap-3 mb-4 flex flex-col items-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Student_icon.svg/1024px-Student_icon.svg.png"
+            alt=""
+            className="w-20 h-20 rounded-full"
+          />
+          <h1>EduAI</h1>
+        </div>
+
+        <NavLink
+          to={""}
+          onClick={() => {
+            setCurrStatus("");
+            dispatch(updateShowSidebar(false));
+          }}
+          className="flex gap-3 items-center w-full"
+        >
+          <IoHomeOutline className="text-3xl" />
           <h1>Home</h1>
-        </div>
-        <div className="flex gap-3 items-center">
-          <AiOutlineTeam />
+        </NavLink>
+        <NavLink
+          to={""}
+          onClick={() => {
+            setCurrStatus("");
+            dispatch(updateShowSidebar(false));
+          }}
+          className="flex gap-3 items-center w-full"
+        >
+          <AiOutlineTeam className="text-3xl" />
           <h1>About Us</h1>
-        </div>
-        <div className="flex gap-3 items-center">
-          <GiBlackBook />
+        </NavLink>
+        <NavLink
+          to={""}
+          onClick={() => {
+            setCurrStatus("");
+            dispatch(updateShowSidebar(false));
+          }}
+          className="flex gap-3 items-center w-full"
+        >
+          <GiBlackBook className="text-3xl" />
           <h1>Courses</h1>
-        </div>
-        <div className="flex gap-3 items-center">
-          <IoBulbOutline />
+        </NavLink>
+        <NavLink
+          to={""}
+          onClick={() => {
+            setCurrStatus("");
+            dispatch(updateShowSidebar(false));
+          }}
+          className="flex gap-3 items-center w-full"
+        >
+          <IoBulbOutline className="text-3xl" />
           <h1>Resourses</h1>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
