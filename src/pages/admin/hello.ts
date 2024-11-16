cb5bafe0965ff8dc291626ab6e5d899f4c721e04
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-16 18:57:02 +0500

    9/11

---
+++ src/pages/admin/hello.ts
@@ -0,0 +1,10 @@
+const myName: String = "hammad";
+const a: Number = 23;
+const b: Number = 32;
+
+function calSum(a: Number, b: Number): Number {
+  const sum: Number = a + b;
+  return sum;
+}
+
+console.log(calSum(a, b));
