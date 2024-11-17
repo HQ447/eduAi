0d0beddbb02d7092ed65e705545b9cafcb1d8a6b
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-17 18:11:41 +0500

    9/11

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
 function Dashboard() {
-  return <div className="flex flex-col w-full px-6 ">Dashboard</div>;
+  return <div className="flex flex-col w-full px-6 py-4 ">Dashboard</div>;
 }
 
 export default Dashboard;
