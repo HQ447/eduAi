8ca095215d6d45d0981c18efa962d70cd2d7e9f8
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-21 20:07:25 +0500

    9/11

--- src/pages/account/AccountSection.jsx
+++ src/pages/account/AccountSection.jsx
@@ -5,7 +5,14 @@ function AccountSection() {
         <div className="Nav  w-full px-20 py-6 -xsm:py-4  -md:px-10 -sm:px-6 -xsm:px-3">
           EduAI
         </div>
-        <div>pic</div>
+        <div className="flex flex-col items-center justify-center w-full">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3781/3781986.png"
+            alt="img loading error"
+            className="bg-white rounded-full w-28"
+          />
+          <h1 className=" text-2xl">Hammad Ahmad</h1>
+        </div>
       </div>
     </div>
   );
