ae08b3a6663167313d415eb1e8577509f760a217
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-21 20:19:26 +0500

    9/11

--- src/pages/account/AccountSection.jsx
+++ src/pages/account/AccountSection.jsx
@@ -5,12 +5,12 @@ function AccountSection() {
 
   return (
     <div className="bg-white w-full min-h-screen">
-      <div className="bg-[#784aeb] flex flex-col ">
-        <div className="Nav flex justify-center text-semibold text-white w-full px-20 py-6 -xsm:py-4  -md:px-10 -sm:px-6 -xsm:px-3">
+      <div className="bg-[#784aeb] pb-32  flex flex-col ">
+        <div className="Nav flex  justify-center text-semibold text-white w-full px-20 py-6 -xsm:py-4  -md:px-10 -sm:px-6 -xsm:px-3">
           EduAI
         </div>
         <hr />
-        <div className="flex flex-col mb-32 font-semibold text-white text-2xl items-center justify-center w-full">
+        <div className="flex flex-col font-semibold text-white text-2xl items-center justify-center w-full">
           <img
             src="https://cdn-icons-png.flaticon.com/512/3781/3781986.png"
             alt="img loading error"
@@ -19,6 +19,9 @@ function AccountSection() {
           <h1 className=" uppercase">{activeUser.username}</h1>
           <p className="text-xl">{activeUser.email}</p>
         </div>
+      </div>
+      <div className="px-20 py-6 -xsm:py-4  -md:px-10 -sm:px-6 -xsm:px-3">
+        ksjd
       </div>
     </div>
   );
