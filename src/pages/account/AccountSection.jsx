08a7a0e40bdf3d81e2efdd4792f0627e4e71c706
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-21 20:02:12 +0500

    9/11

--- src/pages/account/AccountSection.jsx
+++ src/pages/account/AccountSection.jsx
@@ -1,8 +1,11 @@
 function AccountSection() {
   return (
-    <div className="bg-white w-full">
-      <div className="bg-gradient-to-b from-[#653bce] to-[#9966ff] flex flex-col px-20 py-6 -xsm:py-4 items-center -md:px-10 -sm:px-6 -xsm:px-3">
-        <div className="Nav"></div>
+    <div className="bg-white w-full min-h-screen">
+      <div className="bg-[#784aeb] flex flex-col ">
+        <div className="Nav  w-full px-20 py-6 -xsm:py-4  -md:px-10 -sm:px-6 -xsm:px-3">
+          EduAI
+        </div>
+        <div>pic</div>
       </div>
     </div>
   );
