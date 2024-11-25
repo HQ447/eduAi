27ce5b79daa79542ba12e8c2f22948d04d06fea1
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-25 21:42:33 +0500

    34

--- src/pages/account/AccountSection.jsx
+++ src/pages/account/AccountSection.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 
 function AccountSection() {
   const activeUser = useSelector((state) => state.store.activeUser);
   const navigate = useNavigate();
   const [active, setActive] = useState(1);
+  const darkMode = useSelector((state) => state.store.darkMode);
   // Animation Variants
   // const textVariants = {
   //   animate: {
@@ -20,12 +21,49 @@ function AccountSection() {
 
   return (
     <div className="bg-[#dcdcdc] w-full min-h-screen">
-      <div className="bg-[#784aeb] pb-20 -xsm:pb-5 flex flex-col rounded-b-[4rem] -xsm:rounded-b-[2rem]">
-        <div className="Nav flex justify-center text-semibold text-white w-full px-20 py-6 -xsm:py-4 -md:px-10 -sm:px-6 -xsm:px-3">
-          EduAI
+      <div className="bg-[#8971c5] relative pb-14 -xsm:pb-8 flex flex-col gap-6 rounded-b-[4rem] -xsm:rounded-b-[2rem]">
+        <div
+          className={` ${
+            darkMode
+              ? "bg-[#000000d6] text-white"
+              : " bg-white text-[#000000b5] "
+          } flex px-20 py-4 justify-center -xsm:py-3 items-center -md:px-10 -sm:px-6 -xsm:px-3  w-full  transition-all`}
+          style={{
+            boxShadow:
+              "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+          }}
+        >
+          <NavLink to={""} className={"flex items-center gap-1"}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/4729/4729436.png"
+              alt="loading error"
+              className="w-6"
+            />
+            <h1 className="text-xl font-semibold -xsm:text-xl">
+              edu
+              <span
+                style={{
+                  background:
+                    "linear-gradient(90deg, #455be7 2.34%, #C 100.78%)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                AI
+              </span>
+            </h1>
+          </NavLink>
+          <NavLink
+            to={"/"}
+            className={
+              "absolute right-8 -xsm:right-3 text-xs text-white rounded-sm px-2 py-1 bg-[#8971c5]"
+            }
+          >
+            Back
+          </NavLink>
         </div>
 
-        <div className="flex flex-col font-semibold text-white text-2xl items-center justify-center w-full">
+        <div className="flex flex-col -xsm:my-5 font-semibold text-white text-2xl items-center justify-center w-full">
           <img
             src="https://cdn-icons-png.flaticon.com/512/3781/3781986.png"
             alt="img loading error"
@@ -34,13 +72,10 @@ function AccountSection() {
           <h1 className="uppercase -xsm:text-sm">{activeUser.username}</h1>
           <p className="text-xl -xsm:text-sm">{activeUser.email}</p>
         </div>
-      </div>
-
-      <div className="rounded-t-[4rem] -xsm:rounded-t-[2rem] bg-white mt-3 -xsm:mt-2 relative -xsm:py-2 py-12  px-10 -sm:px-16 -xsm:px-7 ">
-        <div className="absolute gap-1 -md:w-[80%] -sm:w-[86%] -xsm:relative -xsm:justify-between -xsm:py-4 -xsm:mb-6 -xsm:top-0 -xsm:w-[95%] flex justify-between rounded-md -top-12 left-0 right-0 shadow-lg shadow-[#c2c2c2] mx-auto py-5 px-7 w-[60%] bg-white">
+        <div className="absolute w-full bottom-0 gap-7 text-white  flex justify-center  ">
           <p
             className={` ${
-              active == 1 ? "border-b-2 border-[#784aeb]" : ""
+              active == 1 ? "border-b-4 border-white" : ""
             } -xsm:text-[10px] `}
             onClick={() => {
               setActive(1);
@@ -51,7 +86,7 @@ function AccountSection() {
           </p>
           <p
             className={` ${
-              active == 2 ? "border-b-2 border-[#784aeb]" : ""
+              active == 2 ? "border-b-4 border-white" : ""
             }  -xsm:text-[10px]`}
             onClick={() => {
               setActive(2);
@@ -62,7 +97,7 @@ function AccountSection() {
           </p>
           <p
             className={` ${
-              active == 3 ? "border-b-2 border-[#784aeb]" : ""
+              active == 3 ? "border-b-4 border-white" : ""
             }  -xsm:text-[10px] `}
             onClick={() => {
               setActive(3);
@@ -72,6 +107,9 @@ function AccountSection() {
             Settings
           </p>
         </div>
+      </div>
+
+      <div className=" bg-white mt-2 -xsm:mt-2 relative -xsm:py-2 py-12  px-10 -sm:px-16 -xsm:px-7 ">
         <Outlet />
       </div>
     </div>
