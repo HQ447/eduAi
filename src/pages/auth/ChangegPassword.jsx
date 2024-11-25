8a4dd4342a3c82f123389ce401ee590779ca9d53
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-25 11:00:21 +0500

    34

--- src/pages/auth/ChangegPassword.jsx
+++ src/pages/auth/ChangegPassword.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function ChangePassword() {
   return (
     <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
-      <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[#ececec] ">
+      <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[white] shadow-2xl ">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Student_icon.svg/1024px-Student_icon.svg.png"
           alt=""
