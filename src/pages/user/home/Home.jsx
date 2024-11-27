dc8f4b3f592c2c46ea0d9a0e0bce8938f83388f0
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-27 17:36:16 +0500

    9/11

--- src/pages/user/home/Home.jsx
+++ src/pages/user/home/Home.jsx
@@ -52,50 +52,52 @@ function Home() {
           </p>
         </div>
         {/* get the purchased courses data of that user */}
-        {courseCollection.map((course, index) => (
-          <div
-            key={index}
-            className={`${
-              index % 2 == 0 ? "flex-row-reverse" : "flex"
-            } flex cursor-pointer -sm:flex-col -sm:w-full w-fit gap-6 border-[1px] border-solid border-gray-500 p-4`}
-            onClick={() => navigate(`/coursedetails/${course.id}`)}
-          >
-            <img
-              src={course.img}
-              alt="img loading error"
-              className="w-[25rem] -l:w-[18rem] -md:w-[15rem] -sm:w-full"
-            />
-            <div className="w-[30rem] -md:w-[25rem] -sm:w-full flex flex-col gap-2">
-              <h1 className="text-xl -sm:text-lg font-semibold">
-                {course.title}
-              </h1>
-              <p className=" text-xs">{course.decription}</p>
-              <p className="text-gray-500 text-xs">By {course.instructor}</p>
-              <p className="text-green-800 text-xs -xsm:text-[8px] font-semibold flex  ">
-                {course.chapters.length} Chapter or Sections
-                <p className="text-gray-500 font-normal">
-                  - {course.lectures} Lectures -
+        {courseCollection
+          .filter((course) => course.popular == true)
+          .map((course, index) => (
+            <div
+              key={index}
+              className={`${
+                index % 2 == 0 ? "flex-row-reverse" : "flex"
+              } flex cursor-pointer -sm:flex-col -sm:w-full w-fit gap-6 border-[1px] border-solid border-gray-500 p-4`}
+              onClick={() => navigate(`/coursedetails/${course.id}`)}
+            >
+              <img
+                src={course.img}
+                alt="img loading error"
+                className="w-[25rem] -l:w-[18rem] -md:w-[15rem] -sm:w-full"
+              />
+              <div className="w-[30rem] -md:w-[25rem] -sm:w-full flex flex-col gap-2">
+                <h1 className="text-xl -sm:text-lg font-semibold">
+                  {course.title}
+                </h1>
+                <p className=" text-xs">{course.decription}</p>
+                <p className="text-gray-500 text-xs">By {course.instructor}</p>
+                <p className="text-green-800 text-xs -xsm:text-[8px] font-semibold flex  ">
+                  {course.chapters.length} Chapter or Sections
+                  <p className="text-gray-500 font-normal">
+                    - {course.lectures} Lectures -
+                  </p>
+                  <p className="text-gray-500 font-normal">All Levels</p>
                 </p>
-                <p className="text-gray-500 font-normal">All Levels</p>
-              </p>
-              <div className="flex items-center gap-1">
-                <p className="font-semibold text-sm">{course.rating}.0</p>
-                <StarRating rating={course.rating} />
-                <p className="text-xs">({course.students})</p>
+                <div className="flex items-center gap-1">
+                  <p className="font-semibold text-sm">{course.rating}.0</p>
+                  <StarRating rating={course.rating} />
+                  <p className="text-xs">({course.students})</p>
+                </div>
+                <p className=" font-semibold">
+                  {course.new_price == 0 ? "Free" : `$${course.new_price}`}
+                  &nbsp;{" "}
+                  <del className="text-red-500 text-sm font-semibold">
+                    ${course.old_price}
+                  </del>
+                </p>
+                <p className="px-2 py-1 text-[#0c4b0c] font-semibold text-xs w-fit rounded-sm bg-[#eceb98]">
+                  Top Rated
+                </p>
               </div>
-              <p className=" font-semibold">
-                {course.new_price == 0 ? "Free" : `$${course.new_price}`}
-                &nbsp;{" "}
-                <del className="text-red-500 text-sm font-semibold">
-                  ${course.old_price}
-                </del>
-              </p>
-              <p className="px-2 py-1 text-[#0c4b0c] font-semibold text-xs w-fit rounded-sm bg-[#eceb98]">
-                Top Rated
-              </p>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       {/* call the backend api for feedbacks , feedbacks will store in database */}
       <Feedback />
