72c2cc0324584b3e439c82a77f2453305288f14b
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-27 17:42:58 +0500

    9/11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,12 +14,11 @@ import {
 
 function Navbar() {
   const dispatch = useDispatch();
+  //
   const showSidebar = useSelector((state) => state.store.showSidebar);
   const darkMode = useSelector((state) => state.store.darkMode);
-
   //I add this to update the active Link in nanvigation
   const currStatus = useSelector((state) => state.store.currStatus);
-
   //After user is uthenticated the user is added to the activeUser obj in store and retrive from store
   const activeUser = useSelector((state) => state.store.activeUser);
   const navigate = useNavigate();
