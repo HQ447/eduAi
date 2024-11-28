b692c29382eaa5b9d2300199e0ea434ca24281c9
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-28 19:29:02 +0500

    kk

--- src/pages/account/AccountSection.jsx
+++ src/pages/account/AccountSection.jsx
@@ -71,6 +71,13 @@ function AccountSection() {
           />
           <h1 className="uppercase -xsm:text-sm">{activeUser.username}</h1>
           <p className="text-xl -xsm:text-sm">{activeUser.email}</p>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#0099ff"
+              // fill-opacity="1"
+              d="M0,256L120,224C240,192,480,128,720,133.3C960,139,1200,213,1320,250.7L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+            ></path>
+          </svg> */}
         </div>
         <div className="absolute w-full bottom-0 gap-7 text-white  flex justify-center  ">
           <p
