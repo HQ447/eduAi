f8f3e96071957251c798c80cb1aeec7b3b6de16d
Hammad Ahmad <hamadqur447@gmail.com>
2024-11-28 20:31:14 +0500

    kk

--- src/pages/account/Navigator.jsx
+++ src/pages/account/Navigator.jsx
@@ -1,5 +1,16 @@
 function Navigator() {
-  return <div>Navigator</div>;
+  return (
+    <div className=" py-10 px-20 min-h-[40vh]">
+      <div className="flex flex-col gap-2 w-fit bg-white p-3 rounded-md">
+        <img
+          src="https://img.freepik.com/free-photo/book-white-background_9975-6513.jpg"
+          alt=""
+          className=" w-32"
+        />
+        <p className="text-sm font-semibold">My Learning</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navigator;
