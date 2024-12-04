001783d307702ae6c03560034ba3655a77ce7b99
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-04 18:48:48 +0500

    4/12

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -13,18 +13,18 @@ function AdminSidebar({ showSidebar }) {
     <div
       className={`${
         showSidebar ? "left-0" : "-left-72"
-      } flex px-5  flex-col w-[22%] -lg:w-[30%] -md:w-[40%] -sm:w-[50%] -xsm:w-[70%] -md:fixed -left-72 bg-white  min-h-screen `}
+      } flex px-5 py-10  flex-col w-[22%] -lg:w-[30%] -md:w-[40%] -sm:w-[50%] -xsm:w-[70%] -md:fixed -left-72 bg-white  min-h-screen `}
     >
-      <div className=" drop-shadow-xl rounded-md  border p-3">
+      <div className="relative pt-14 border shadow-md rounded-md text-center   p-3">
         <img
           src="https://imgcdn.stablediffusionweb.com/2024/6/12/4d688bcf-f53b-42b6-a98d-3254619f3b58.jpg"
           alt=""
-          className="rounded-full w-16 h-16"
+          className="rounded-full w-16 h-16 absolute -top-5 left-20"
         />
-        <h1>Ayesha Khan</h1>
+        <h1 className=" font-semibold">Ayesha Khan Afridi</h1>
         <p className=" text-xs">Founder of EduAI</p>
       </div>
-      <div className="flex flex-col my-10 gap-3">
+      <div className="flex flex-col my-5 gap-3">
         <NavLink
           to={""}
           className={`${
