ab46422171a5386b8d8437b4d334f1055df086cf
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-04 19:40:26 +0500

    4/12

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import AdminSidebar from "../../components/AdminSidebar";
 import { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
+import { SlMagnifier } from "react-icons/sl";
 
 function AdminDashboard() {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -22,7 +23,14 @@ function AdminDashboard() {
               setShowSidebar((prev) => !prev);
             }}
           />
-          profileicon
+          <div className="flex items-center gap-2">
+            <SlMagnifier />
+            <input
+              type="text"
+              placeholder="Search now"
+              className=" placeholder:text-sm placeholder:text-gray-300 outline-none"
+            />
+          </div>
         </div>
         <div className="flex flex-col bg-[#f5f7ff] -xsm:px-3 px-10 py-4 min-h-screen ">
           <Outlet />
