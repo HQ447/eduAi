6714c1cc32e59606c6d3358cb3adf007ee13d1f5
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-13 20:56:47 +0500

    final commit

--- src/pages/admin/ManageCourses.jsx
+++ src/pages/admin/ManageCourses.jsx
@@ -3,7 +3,10 @@ import { SlMagnifier } from "react-icons/sl";
 //Retrive all courses from DB in useEffect , and then dispatch it into redux store to prevent api calls
 import { courseCollection } from "../../Data/data";
 import { useState } from "react";
-import StarRating from "../../components/StarRating";
+// import StarRating from "../../components/StarRating";
+import { ImBin } from "react-icons/im";
+import { FaRegEdit } from "react-icons/fa";
+
 function ManageCourses() {
   const [searchInp, setSearchInp] = useState("");
 
@@ -16,7 +19,7 @@ function ManageCourses() {
   });
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="flex items-center justify-between w-full">
         <div className="flex items-center  gap-2 py-2 px-2 rounded-full bg-white">
           <SlMagnifier />
@@ -32,36 +35,41 @@ function ManageCourses() {
         </button>
       </div>
 
-      <div>
+      <div className=" flex flex-wrap gap-2">
         {filteredCourses.map((obj) => (
           <div
             key={obj.id}
-            className={`flex p-[14px]   w-72 -xsm:w-full flex-col -xsm:text-sm  cursor-pointer hover:scale-95 hover:shadow-xl transition-all
+            className={`flex relative p-[14px] bg-white w-48 !text-xs -xsm:w-full flex-col -xsm:text-sm   shadow transition-all
             `}
           >
+            <div className="absolute  hover:scale-95 transition-all cursor-pointer top-5 right-5 border px-2 py-2 bg-white rounded-md">
+              <ImBin className="   text-lg text-[red]" />
+            </div>
+            <div className="absolute  hover:scale-95 transition-all cursor-pointer top-16 right-5 border px-2 py-2 bg-white rounded-md">
+              <FaRegEdit className="   text-lg " />
+            </div>
+
             <img
               src={obj.img}
               className="w-full "
               alt="cource img loading error "
             />
             <div className="pt-3 flex flex-col gap-2">
-              <h1 className="text-[15px] -xsm:text-sm text-black font-semibold">
-                {obj.title}
-              </h1>
-              <p className="text-xs text-[#515151]">{obj.decription}</p>
-              <p className="text-xs text-green-800">{obj.instructor}</p>
+              <h1 className=" text-black font-semibold">{obj.title}</h1>
+              {/* <p className="text-xs text-[#515151]">{obj.decription}</p> */}
+              {/* <p className="text-xs text-green-800">{obj.instructor}</p> */}
               <p className="text-[10px] text-gray-400">
                 {obj.chapters.length} Sections - {obj.lectures.length} Lectures
               </p>
-              <div className="flex items-center text-sm">
-                <h1 className="font-semibold text-sm">{obj.rating}.0</h1>
+              {/* <div className="flex items-center text-xs">
+                <h1 className="font-semibold text-xs">{obj.rating}.0</h1>
                 &nbsp;
                 <StarRating rating={obj.rating} />
                 &nbsp;
                 <p>({obj.students})</p>
-              </div>
+              </div> */}
               <div className="flex justify-between items-center">
-                <p className="font-semibold text-lg">
+                <p className="font-semibold text-sm">
                   {obj.new_price == 0 ? "Free" : `$${obj.new_price}`}
                   &nbsp;{" "}
                   <del className="text-red-500 text-sm font-semibold">
