33235a71c517cab714d09d1f973ff8aa1be40234
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-13 20:25:28 +0500

    final commit

--- src/pages/admin/ManageCourses.jsx
+++ src/pages/admin/ManageCourses.jsx
@@ -2,16 +2,19 @@ import { SlMagnifier } from "react-icons/sl";
 
 function ManageCourses() {
   return (
-    <div>
-      <div className="flex items-center ">
-        <div className="flex items-center gap-2 py-2 px-2 rounded-full bg-[#edf1fc]">
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center  gap-2 py-2 px-2 rounded-full bg-white">
           <SlMagnifier />
           <input
             type="text"
             placeholder="Search now"
-            className="text-xs bg-[#edf1fc] placeholder:text-[#7f7f7f]  placeholder:text-xs  outline-none"
+            className="text-xs  bg-white placeholder:text-[#7f7f7f]  placeholder:text-xs  outline-none"
           />
         </div>
+        <button className="text-xs bg-[#464646] hover:scale-95 transition-all px-3 py-[5px] rounded-md text-white">
+          Add new Course
+        </button>
       </div>
     </div>
   );
