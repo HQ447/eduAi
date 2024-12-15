4ed58124f4b29ea584c850f1d5d22e1ec68d526a
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-15 08:11:25 +0500

    final commit

--- src/pages/admin/ManageCourses.jsx
+++ src/pages/admin/ManageCourses.jsx
@@ -39,7 +39,7 @@ function ManageCourses() {
         {filteredCourses.map((obj) => (
           <div
             key={obj.id}
-            className={`flex relative p-[14px] -xsm:p-[10px] bg-white w-48 -xsm:w-36 !text-xs  flex-col -xsm:text-sm   shadow transition-all
+            className={`flex relative p-[14px] -xsm:p-[10px] bg-white w-48 -xsm:w-[10rem] !text-xs  flex-col -xsm:text-sm   shadow transition-all
             `}
           >
             <div className="absolute  hover:scale-95 transition-all cursor-pointer top-5 -xsm:top-4 right-5 -xsm:right-3 border px-2 py-2 bg-white rounded-md">
@@ -78,6 +78,16 @@ function ManageCourses() {
                     ${obj.old_price}
                   </del>
                 </p>
+              </div>
+              <div className="flex w-full gap-2 justify-between">
+                <button className="hover:bg-[#252525] hover:!text-white hover:scale-95 transition-all w-full justify-center py-1 -xsm:text-[8px] rounded-md flex gap-1 border-black bg-white border">
+                  <FaRegEdit className="   text-sm -xsm:text-xs " />{" "}
+                  <p className="text-[10px]">Update</p>
+                </button>
+                <button className=" hover:bg-[#dd0303] hover:!text-white hover:scale-95 transition-all w-full justify-center  py-1 -xsm:text-[8px] text-[red] border-black rounded-md flex gap-1 bg-white border">
+                  <ImBin className="   text-sm -xsm:text-xs " />
+                  <p className="text-[10px]">Delete</p>
+                </button>
               </div>
             </div>
           </div>
