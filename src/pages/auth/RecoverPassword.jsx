d79bb888afda67c500df27408302e0167aab873e
Hammad Ahmad <hamadqur447@gmail.com>
2025-04-14 20:55:42 +0500

    ss

--- src/pages/auth/RecoverPassword.jsx
+++ src/pages/auth/RecoverPassword.jsx
@@ -6,23 +6,23 @@ function RecoverPassword() {
   console.log(email);
 
   return (
-    <div className=" backdrop-blur-sm flex w-full min-h-screen justify-center items-center">
+    <div className="flex items-center justify-center w-full min-h-screen backdrop-blur-sm">
       <div className=" w-[28%] -xl:w-[35%] -lg:w-[45%] -md:w-[60%] -sm:w-[70%] -xsm:w-full -xsm:h-full -xsm:justify-center flex flex-col px-10 py-10 rounded-lg bg-[white] shadow-2xl ">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Student_icon.svg/1024px-Student_icon.svg.png"
           alt=""
-          className="w-14 h-14 rounded-full mb-2 mx-auto"
+          className="mx-auto mb-2 rounded-full w-14 h-14"
         />
-        <h1 className="text-lg text-center font-semibold mt-3">
+        <h1 className="mt-3 text-lg font-semibold text-center">
           Recover Your Password!
         </h1>
-        <p className="text-md text-center mb-3">with simple 2 steps</p>
-        <div className="flex flex-col my-3 gap-4">
+        <p className="mb-3 text-center text-md">with simple 2 steps</p>
+        <div className="flex flex-col gap-4 my-3">
           <input
             type="text"
             placeholder="Enter Email"
             onChange={(e) => setEmail(e.target.value)}
-            className="border shadow-md px-3   outline-none  py-3 placeholder:text-sm rounded-md placeholder:text-gray-400"
+            className="px-3 py-3 border rounded-md shadow-md outline-none placeholder:text-sm placeholder:text-gray-400"
           />
         </div>
         <NavLink to={"/get-otp"} className={"w-full"}>
@@ -30,9 +30,18 @@ function RecoverPassword() {
             Next
           </button>
         </NavLink>
+        <p className="text-[12px] text-center ">
+          Back to?{" "}
+          <NavLink
+            to={"/signup"}
+            className={"text-blue-700 hover:font-semibold cursor-pointer"}
+          >
+            Login
+          </NavLink>
+        </p>
       </div>
 
-      {/* <div className=" w-fit flex justify-center items-center gap-8 flex-col bg-white px-20 -md:px-16 -sm:px-8 -xsm:px-2 -xsm:py-3 py-10 -xsm:w-11/12 rounded-lg">
+      {/* <div className="flex flex-col items-center justify-center gap-8 px-20 py-10 bg-white rounded-lg w-fit -md:px-16 -sm:px-8 -xsm:px-2 -xsm:py-3 -xsm:w-11/12">
 
       <h1 className="w-fit text-center shadow-lg rounded-full px-9 py-1 bg-[#fbb329] text-xl ">
         Login
@@ -41,13 +50,13 @@ function RecoverPassword() {
         <img
           src="https://sindphanapublicschool.com/Assets/img/logo-2.png"
           alt="login imag error"
-          className=" w-60 -md:w-40 -md:h-40 mx-auto "
+          className="mx-auto w-60 -md:w-40 -md:h-40"
         />
-        <div className=" w-full items-center justify-center gap-2 flex flex-col px-5 -xsm:px-0">
+        <div className="flex flex-col items-center justify-center w-full gap-2 px-5 -xsm:px-0">
           <div className="border-b-2 border-[#fbb329] flex items-center">
             <input
               type="text"
-              className=" py-4 px-1 outline-none placeholder:text-black placeholder:text-lg"
+              className="px-1 py-4 outline-none placeholder:text-black placeholder:text-lg"
               placeholder="Email ID"
             />
             <FaUser className="text-3xl" />
@@ -55,7 +64,7 @@ function RecoverPassword() {
           <div className="border-b-2 border-[#fbb329] flex items-center">
             <input
               type="password"
-              className=" py-4 px-1 outline-none placeholder:text-black placeholder:text-lg"
+              className="px-1 py-4 outline-none placeholder:text-black placeholder:text-lg"
               placeholder="Password"
             />
             <FaLock className="text-3xl" />
